4f110f2aadc118dd0c322cd0cc3103b76939a12e
Mark Ryan <markryandev@gmail.com>
2017-04-25 20:23:38 -0600

    added simple list (frontend only)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  name = "Sally Salamander"
+  tasks = [
+    {text:"clean computers", done:false},
+    {text:"make invoice", done:false},
+    {text:"email Frank", done:false},
+  ]
+  remove(taskToRemove){
+    this.tasks = this.tasks.filter(task => task != taskToRemove);
+  }
+  add(taskToAdd){
+    this.tasks = [...this.tasks, {text:taskToAdd, done:false}];
+  }
 }
