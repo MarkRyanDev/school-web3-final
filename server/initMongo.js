fc03b27bc7524c2018005497b77fff8e0ef4e63b
Mark Ryan <markryandev@gmail.com>
2017-04-29 13:28:14 -0600

    restructured database to seperate employees and tasks

--- server/initMongo.js
+++ server/initMongo.js
@@ -7,31 +7,29 @@ mongo.connect(url, function(err, db){
     var collection = db.collection('employees');
     collection.insertMany([
         {
-            name: "Sally Salamander",
-            tasks:[
-                {text:"clean computers", done:true},
-                {text:"email frank", done:false},
-                {text:"make invoice", done:false}
-            ]
+            name: "Sally Salamander"
         },
         {
-            name: "John Johnson",
-            tasks:[
-                {text:"clean toilet", done:false},
-                {text:"take out trash", done:false},
-                {text:"mop floors", done:false}
-            ]
+            name: "John Johnson"
         },
         {
-            name: "Catbert",
-            tasks:[
-                {text:"Fire Dilbert", done:false},
-                {text:"Cackle", done:true},
-                {text:"Yell at Wally", done:false}
-            ]
+            name: "Catbert"
         }
     ], function(err, result){
         // console.log(result);
-        db.close();
+        var taskCollection = db.collection('tasks');
+        taskCollection.insertMany([
+            {text:"clean computers", done:true, empid:result.insertedIds[0]},
+            {text:"email frank", done:false, empid:result.insertedIds[0]},
+            {text:"make invoice", done:false, empid:result.insertedIds[0]},
+            {text:"clean toilet", done:false, empid:result.insertedIds[1]},
+            {text:"take out trash", done:false, empid:result.insertedIds[1]},
+            {text:"mop floors", done:false, empid:result.insertedIds[1]},
+            {text:"Fire Dilbert", done:false, empid:result.insertedIds[2]},
+            {text:"Cackle", done:true, empid:result.insertedIds[2]},
+            {text:"Yell at Wally", done:false, empid:result.insertedIds[2]}
+        ], function(err, result){
+            db.close();
+        })
     })
 })
--- server/mongo-dao.js
+++ server/mongo-dao.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 var mongoall = require('mongodb');
 var mongo = mongoall.MongoClient;
 var o_id = mongoall.ObjectID;
@@ -13,11 +14,29 @@ exports.getAll = function(callback){
 
         var collection = db.collection('employees');
 
+        collection.find({}).toArray(function(err, emps){
+            var taskCollection = db.collection('tasks');
+            taskCollection.find({}).toArray(function(err, tasks){
+                var groupedTasks = _.groupBy(tasks, 'empid');
+                emps.forEach(emp => emp.tasks = groupedTasks[emp._id]);
+                db.close();
+                callback(err, emps);
+            });
+        });
+    });
+};
+
+exports.deleteTask = function(empid, taskid, callback){
+    mongo.connect(url, function(err, db){
+        if(err)callback(err);
+
+        var collection = db.collection('employees');
+
         collection.find({}).toArray(function(err, docs){
             db.close();
             callback(err, docs);
         });
     });
-};
+}
 
 module.exports = exports;
