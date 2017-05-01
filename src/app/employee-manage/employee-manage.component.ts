0a37776650c21baff9a982a8ef8ff6a57a9d4ca5
Brock Ayres <brockayres82@gmail.com>
2017-05-01 15:27:28 -0600

    multiple add functionality to employer view

--- src/app/employee-manage/employee-manage.component.ts
+++ src/app/employee-manage/employee-manage.component.ts
@@ -7,28 +7,35 @@ import { Http } from '@angular/http';
   styleUrls: ['./employee-manage.component.css']
 })
 export class EmployeeManageComponent {
-  constructor(private http: Http) { }
+  constructor(private http: Http) {
+  }
 
   @Input() emp
-  remove(taskToRemove){
+
+  remove(taskToRemove) {
     this.http.delete('/api/tasks/' + taskToRemove._id)
-               .toPromise()
-               .then(()=>this.emp.tasks = this.emp.tasks.filter(task => task != taskToRemove))
-               .catch(function(err){console.log(err);});
+      .toPromise()
+      .then(() => this.emp.tasks = this.emp.tasks.filter(task => task != taskToRemove))
+      .catch(function (err) {
+        console.log(err);
+      });
   }
-  add(taskToAdd){
-    let wasRecurring  = this.newItemIsRecurring;
+
+  add(taskToAdd) {
+    let wasRecurring = this.newItemIsRecurring;
     this.newItemIsRecurring = false;
-    this.http.post('/api/tasks', {text:taskToAdd, done:false, empid:this.emp._id, recurring:wasRecurring})
-               .toPromise()
-               .then(response=>{
-                 this.emp.tasks =
-                  [
-                    ...this.emp.tasks,
-                    {text:taskToAdd, done:false, empid:this.emp._id, _id:response.json(), recurring:wasRecurring}
-                  ];
-                })
-               .catch(function(err){console.log(err);});
+    this.http.post('/api/tasks', {text: taskToAdd, done: false, empid: this.emp._id, recurring: wasRecurring})
+      .toPromise()
+      .then(response => {
+        this.emp.tasks =
+          [
+            ...this.emp.tasks,
+            {text: taskToAdd, done: false, empid: this.emp._id, _id: response.json(), recurring: wasRecurring}
+          ];
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   }
 
   newItemIsRecurring = false;
